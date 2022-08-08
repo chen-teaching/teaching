7171670ebc840ec7afc995487e3b80116c075575
Tyler <chentylerl@gmail.com>
2022-08-08 08:00:35 -0400

    add preliminary files for Fall 2022

---
+++ build_site.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import re
+import os
+os.environ['PATH'] += os.pathsep + '/home/tyler/anaconda3/bin/'
+
+
+def build_html(file_name):
+    """
+    generate html file from markdown
+    """
+
+    print(f'now building {file_name}')
+
+    # convert md to html
+
+    options = ['--from markdown+tex_math_single_backslash-auto_identifiers',
+               '--to html5',
+               '--wrap=preserve',
+               '--standalone ',
+               '--mathjax',
+               f'--data-dir={os.getcwd()}',
+               f'--variable depth={"../"*file_name.count("/")}'
+               ]
+
+    os.system(f'pandoc {" ".join(options)} -o {file_name}.html {file_name}.md --template template.html')
+    #os.system(f'pandoc -o {file_name}.pdf src/{file_name}.md')
+
+
+build_html('index')
+build_html('mathstats_f2022/index')
+build_html('mathstats_f2022/syllabus')
+
---
+++ index.md
@@ -0,0 +1,20 @@
+---
+title:  Courses
+keywords: ['courses','teaching','applied','math']
+description: Here is info on my teaching.
+...
+
+
+## NYU
+
+- [Mathematical Statistics (Fall 2022)](./mathstats_f2022)
+
+## UW
+
+- Applied Linear Algebra and Numerical Analysis (Spring 2021)
+- Interdisciplinary Writing/Natural Science (Winter 2021)
+- Interdisciplinary Writing/Natural Science (Fall 2020)
+
+
+You can email me at [chentyl@uw.edu](mailto:chentyl@uw.edu).
+Pronouns: they or he.
